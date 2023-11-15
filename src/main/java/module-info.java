5c203f8e617370efbe39df0ff54ebd1e7706cc36
Niccolò <niccoredi@gmail.com>
2023-11-15 18:38:08 +0100

    Added DB support and a class for the connection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,11 @@ module com.unifysweproject.hotelsupplymanagement {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires org.slf4j;
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
+    requires org.xerial.sqlitejdbc;
+    requires java.sql;
 
     opens com.unifisweproject.hotelsupplymanagement to javafx.fxml;
     exports com.unifisweproject.hotelsupplymanagement;
